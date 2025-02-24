cdd22d9b68c4436beea5ed3a92c362729fcbfab3
Chethan Karkala Shettigar <chethan.shettigar@valtech.com>
2025-02-24 14:29:13 +0530

    Added Product and Product detail models

---
+++ InventoryManagement.Models/Product.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventoryManagement.Models
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public decimal Price { get; set; }
+        public int StockQuantity { get; set; }
+        public int SellerId { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
---
+++ InventoryManagement.Models/ProductDetail.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventoryManagement.Models
+{
+    public class ProductDetail
+    {
+        public int ProductDetailId { get; set; }
+        public string Description { get; set; } = string.Empty;
+        public string ProductPhoto { get; set; } = string.Empty;
+        public int ProductId { get; set; }
+    }
+}

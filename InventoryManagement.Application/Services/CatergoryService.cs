4697b4114535bd3559974e71ce1ac1c3cc9b2f11
Chethan Karkala Shettigar <chethan.shettigar@valtech.com>
2025-02-25 11:37:06 +0530

    Added Supplier Service and Repository and Catergpry Service Added Product Add page and detail page

---
+++ InventoryManagement.Application/Services/CatergoryService.cs
@@ -0,0 +1,23 @@
+﻿using InventoryManagement.Application.Interfaces;
+using InventoryManagement.Models.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventoryManagement.Application.Services
+{
+    public class CatergoryService
+    {
+        private readonly ICategoryService _categoryService;
+        public CatergoryService(ICategoryService categoryService)
+        {
+            _categoryService= categoryService;
+        }
+        public async Task<IEnumerable<Categories>> GetAllCategories()
+        {
+            return await _categoryService.GetAllCategories();
+        }
+    }
+}
---
+++ InventoryManagement.Repo/Repository/SupplierRepository.cs
@@ -0,0 +1,34 @@
+﻿using Dapper;
+using InventoryManagement.Models.Entities;
+using InventoryManagement.Repo.Data;
+using InventoryManagement.Repo.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventoryManagement.Repo.Repository
+{
+    public class SupplierRepository : ISupplierRepository
+    {
+        private readonly DapperDbContext _dbContext;
+        public SupplierRepository(DapperDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<IEnumerable<Suppliers>> GetAllSuppliers()
+        {
+            using var connection = _dbContext.CreateConnection();
+            return await connection.QueryAsync<Suppliers>("SELECT * FROM Suppliers");
+        }
+
+        public async Task<Suppliers> GetSuppliersById(int id)
+        {
+            using var connection = _dbContext.CreateConnection();
+            return await connection.QueryFirstOrDefaultAsync<Suppliers>(
+                "SELECT * FROM Suppliers WHERE SupplierId = @SupplierId", new { SupplierId = id }) ?? new Suppliers();
+        }
+    }
+}

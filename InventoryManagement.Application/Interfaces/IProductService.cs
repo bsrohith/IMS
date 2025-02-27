3e29a26bceec168f0e71e68d67014287982503ba
RohithVal <rohith.bukkambudhi@valtech.com>
2025-02-27 17:23:43 +0530

    remove defn

--- InventoryManagement.Application/Interfaces/IProductService.cs
+++ InventoryManagement.Application/Interfaces/IProductService.cs
@@ -19,7 +19,7 @@ namespace InventoryManagement.Application.Interfaces
         Task<ProductViewModel> GetProductViewModeByIdAsync(int id);
         Task UpdateProductStockQuantity(int productid, int quantity);
         Task<Suppliers> GetSupplierByUserIdAsync(int userId);
-        Task<List<ProductViewModel>> GetAllProductsWithDetailWithUserAsync(int userId);  //remove this
+
 
     }
 }
